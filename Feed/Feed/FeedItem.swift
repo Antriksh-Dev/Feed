af67f5af9918242e3d82f59dce524c7f9f243181
Antriksh Verma <connecttoantriksh@gmail.com>
2023-07-04 10:05:15 -0700

    Create FeedItem model

---
+++ Feed/Feed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  Feed
+//
+//  Created by Antriksh Verma on 7/4/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let url: URL
+}
